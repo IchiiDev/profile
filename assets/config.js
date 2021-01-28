3a3a6ed9f6bea55393fa3e1e688d7ec5855862f8
Ichii <45918948+IchiiDev@users.noreply.github.com>
2021-01-28 21:08:00 +0100

    Edit config and assets for public use

---
+++ assets/config.js
@@ -0,0 +1,24 @@
+const config = {
+    
+    USERNAME: "User#0000",
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: "Change this bio in the <b>assets/config.js</b> file. <i>It may include HTML syntax</i>",
+
+    BADGES: [
+        { id: "HypeSquad Bravery", image: "assets/img/badge_hypesquad_bravery.svg" },
+        { id: "Nitro Sub", image: "assets/img/badge_nitro.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com", name: "Super cool project 1" },
+        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.com", name: "Super cool project 2" },
+        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com", name: "Super cool project 3" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/discord" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/discord" },
+    ],
+
+}
