3b71bded2753dddd42e1d11d4513be5b5c12ca46
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2021-10-10 22:19:55 +0200

    Implements gravatar

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,17 @@
+function getPicture() {
+    if (config.PICTURE.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+        let hash = CryptoJS.MD5(config.PICTURE);
+        return `https://gravatar.com/avatar/${hash}?s=512&d=mp`;
+    } else if (config.PICTURE.match(/[a-fA-F0-9]{32}/)) {
+        return `https://gravatar.com/avatar/${config.PICTURE}?s=512&d=mp`;
+    } else return config.PICTURE;
+}
+
 let body = new Vue({
     el: '.profile',
     data: {
         username: config.USERNAME,
-        profile_picture: config.PICTURE,
+        profile_picture: getPicture(),
         badges: config.BADGES,
         description: config.DESCRIPTION,
         projects: config.PROJECTS,
